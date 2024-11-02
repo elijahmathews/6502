e1393c6189406f17e1985a2f257758418108d1f3
Elijah Mathews <pgp@elijahmathews.com>
2024-11-02 17:04:12 -0400

    init code files

---
+++ src/main.s
@@ -0,0 +1,10 @@
+.setcpu "65C02"
+
+.segment "CODE"
+reset:
+	nop		; Don't actually do anything
+	jmp	reset	; Go back to the nop instruction
+
+.segment "RESET"
+	.word reset	; Go to the reset subroutine
+	.word $0000
