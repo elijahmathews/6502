2bef5b5cd60e1de62e6bf9b1a844ec98a1a423ef
Elijah Mathews <pgp@elijahmathews.com>
2024-11-02 17:26:46 -0400

    clarify what is going on a bit better

--- src/main.s
+++ src/main.s
@@ -1,9 +1,14 @@
 .setcpu "65C02"
 
 .segment "CODE"
-reset:
-	nop		; Don't actually do anything
-	jmp	reset	; Go back to the nop instruction
 
-.segment "RESET"
-	.word	reset	; Go to the reset subroutine
+reset:
+	nop		; Don't actually do anything.
+	jmp	reset	; Go back to the NOP instruction.
+
+.segment "RESET"	; Reset requires special handling.
+			; Execution starts at address stored
+			; at address FFFC.
+
+resetvec:
+	.word	reset	; Go to the reset label.
